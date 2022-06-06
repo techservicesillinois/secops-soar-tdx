8b3e393de270b4ffa382c93f91e1733841a396fb
Tyler Turner <tzturner@illinois.edu>
2022-06-06 11:48:58 -0500

    Yet MORE build stuff!

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+import logging
+import pathlib
+
+import pytest
+import vcr
+
+from phsoar_null_router.soar_null_router_connector import Soar_Null_RouterConnector
+
+# Required pytest plugins
+pytest_plugins = ("splunk-soar-connectors")
+
+
+@pytest.fixture
+def connector(monkeypatch) -> Soar_Null_RouterConnector:
+    monkeypatch.setenv("BHR_HOST", "https://nr-test.techservices.illinois.edu")
+    monkeypatch.setenv("BHR_TOKEN", "FAKETOKEN")
+
+    conn = Soar_Null_RouterConnector()
+    conn.logger.setLevel(logging.INFO)
+    return conn
+
+
+@pytest.fixture
+def cassette(request) -> vcr.cassette.Cassette:
+    my_vcr = vcr.VCR(
+        cassette_library_dir='cassettes',
+        record_mode='once',
+    )
+
+    with my_vcr.use_cassette(f'{request.function.__name__}.yaml') as tape:
+        yield tape
---
+++ tests/test_tdx_connection.py
@@ -0,0 +1,7 @@
+import tdxlib
+import vcrpy
+
+
+def test_tdx_connection(cassette):
+    tdx = tdxlib.tdx_ticket_integration.TDXTicketIntegration('tdxlib.ini')
+    search = tdx.search_tickets("test")
