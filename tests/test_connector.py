c58bacaf241ce659292d99fae4b1728b2ddc78ee
Tyler Turner <tzturner@illinois.edu>
2022-06-30 14:59:34 -0500

    Passing create_ticket test

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -53,9 +53,11 @@ def test_create_ticket(cassette, connector: TdxConnector):
     }
 
     # with pytest.raises(HTTPError):
+    # import pdb; pdb.set_trace()
     result = json.loads(connector._handle_action(json.dumps(in_json), None))
 
-    # assert response.ticket_data['ID'] == response.get_id()
+    assert result[0]["data"][0]["ID"] == 216792
+    assert result[0]["data"][0]["Title"] == in_json["parameters"][0]["title"]
     assert result[0]["message"] == f"New ticket created"
     assert cassette.all_played  # Move to conftest.py
     # assert cassette.play_count == 1
