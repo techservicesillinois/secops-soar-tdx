d079256ae513564c953d067067f1c69e5886e5a3
Tyler Turner <tzturner@illinois.edu>
2022-10-26 16:59:33 +0000

    WIP

--- tests/conftest.py
+++ tests/conftest.py
@@ -53,7 +53,7 @@ def remove_token(response):
 def cassette(request) -> vcr.cassette.Cassette:
     my_vcr = vcr.VCR(
         cassette_library_dir='cassettes',
-        record_mode='once',
+        record_mode='none', # Use 'once' in development, 'none' when done
         before_record_request=remove_creds,
         before_record_response=remove_token,
         filter_headers=[('Authorization', 'Bearer FAKE_TOKEN')]
@@ -61,5 +61,5 @@ def cassette(request) -> vcr.cassette.Cassette:
 
     with my_vcr.use_cassette(f'{request.function.__name__}.yaml') as tape:
         yield tape
-        assert tape.all_played
+        assert tape.all_played, f"Only played back {len(tape.responses)} responses"
         assert tape.play_count == 1
