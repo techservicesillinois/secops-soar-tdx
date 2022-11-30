5b0f444be77be400d69affd20138eca35c12f7ae
Tyler Turner <tzturner@illinois.edu>
2022-11-30 10:57:02 -0600

    Clean the JWT token (#10)
    - Add VCRMODE variable to ease switching between record and playback
    
    Closes #9

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
+import gzip
 import json
+import jwt
 import logging
 import os
-import pathlib
 
 import pytest
 import vcr
@@ -13,6 +14,7 @@ pytest_plugins = ("splunk-soar-connectors")
 
 CASSETTE_USERNAME = "FAKE_USERNAME"
 CASSETTE_PASSWORD = "FAKE_PASSWORD"
+VCRMODE = 'none'  # Use 'once' in development, 'none' when done
 
 
 @pytest.fixture
@@ -24,6 +26,11 @@ def connector(monkeypatch) -> TdxConnector:
         "TDX_USERNAME": os.environ.get('TDX_USERNAME', CASSETTE_USERNAME),
         "TDX_PASSWORD": os.environ.get('TDX_PASSWORD', CASSETTE_PASSWORD),
     }
+    if VCRMODE == 'none':  # Always use cassette values when using cassette
+        conn.config = {
+            "TDX_USERNAME": CASSETTE_USERNAME,
+            "TDX_PASSWORD": CASSETTE_PASSWORD,
+        }   
     conn.logger.setLevel(logging.INFO)
     return conn
 
@@ -46,9 +53,9 @@ def remove_token(response):
     if not "body" in response:
         return response
     if "'string': b'" in str(response["body"]):
-        with open('./cassettes/expired_token.txt', 'r') as f:
-            response["body"]["string"] = \
-                bytes("".join(f.read().splitlines()), "ascii")
+        token = \
+            jwt.encode({'exp':'secretssecrets'},'arenofun', algorithm='HS256')
+        response['body']['string'] = gzip.compress(bytes(token, "ascii"))
     return response
 
 
@@ -56,9 +63,9 @@ def remove_token(response):
 def cassette(request) -> vcr.cassette.Cassette:
     my_vcr = vcr.VCR(
         cassette_library_dir='cassettes',
-        record_mode='none',  # Use 'once' in development, 'none' when done
-        # before_record_response=remove_token,
-        filter_headers=[('Authorization', 'Bearer FAKE_TOKEN')],
+        record_mode=VCRMODE,
+        before_record_request=remove_creds,
+        before_record_response=remove_token,
         match_on=['uri', 'method'],
     )
     my_vcr.allow_playback_repeats = True # Required due to double Auth()
