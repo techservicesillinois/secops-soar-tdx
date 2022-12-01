8c2aa190095262debdfa3868a891d05b56ae9025
Edward Delaporte <edward@delaporte.us>
2022-12-01 17:57:45 +0000

    WIP - not working - cleaner function attempts

--- tests/conftest.py
+++ tests/conftest.py
@@ -49,17 +49,59 @@ def remove_creds(request):
     request.body = json.dumps(data)
     return request
 
+import re
+from typing import Union
+
+def json_sanitize(value: Union[str, dict, list], is_value=True) -> Union[str, dict, list]:
+    """
+    Modified version of https://stackoverflow.com/a/45526935/2635443
+
+    Recursive function that allows to remove any special characters from json, especially unknown control characters
+    """
+    if isinstance(value, dict):
+        # TODO: Add check that 'string' is in bytes
+        if 'string' in value:
+            import pdb; pdb.set_trace()
+            result = json_sanitize(json.loads(value['string'].decode()))
+            value['string'] = bytes(result, 'ascii')
+            return value
+
+        # TODO: Lots more keys
+        clean_keys = ['Phone', 'PrimaryEmail']
+
+        # for key in clean_keys:
+        # import pdb; pdb.set_trace()
+        if 'PrimaryEmail' in value:
+            value[key] = 'CLEANED'
+
+        value = {json_sanitize(k, False):json_sanitize(v, True) for k, v in value.items()}
+    elif isinstance(value, list):
+        value = [json_sanitize(v, True) for v in value]
+    elif isinstance(value, str):
+        pass
+    return value
+
+def clean_json_string(json_string):
+    import re
+    finder = re.compile('"PrimaryEmail":.*,')
+    clean_string = re.sub(finder, '"PrimaryEmail": "CLEANED",', json_string)
+    return clean_string
 
 def remove_token(response):
     if not "body" in response:
         return response
     # import pdb; pdb.set_trace()
+
     # 'Content-Encoding: gzip' is a hack for trouble decoding the JWT
     if 'Content-Encoding' in response['headers'].keys() and \
         response['headers']['Content-Encoding'] == ['gzip']:
         token = \
             jwt.encode({'exp':datetime.datetime(2049, 6, 25)},'arenofun', algorithm='HS256')
         response['body']['string'] = gzip.compress(bytes(token, "ascii"))
+    
+    response['body'] = json_sanitize(response['body'])
+    # response['body']['string'] = bytes(clean_json_string(str(response['body']['string'])), 'ascii')
+    
     return response
 
 
