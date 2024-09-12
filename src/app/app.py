74d9af9b9c4b95501ba17f4c8daf0208e6c395eb
Edward Delaporte <delaport@illinois.edu>
2024-09-12 15:01:28 -0500

    FIX - BAD SPACE NO DEPLOY

--- src/app/app.py
+++ src/app/app.py
@@ -70,7 +70,7 @@ class TdxConnector(BaseConnector):
 
         # Call the BaseConnectors init first
         super(TdxConnector, self).__init__()
-        self.actions = {}  # Attempt to break  `app.json` parsing on SOAR 
+        self.actions = {}  # Attempt to break  `app.json` parsing on SOAR
         self._state = None
 
     def _handle_test_connectivity(self, param):
