474fd6c951a92f00c7f996941e3edb26d8e881e4
Zach Carrington <zdc@illinois.edu>
2024-09-23 16:37:57 +0000

    Remove BaseConnector

--- src/app/app.py
+++ src/app/app.py
@@ -8,16 +8,14 @@
 from __future__ import print_function, unicode_literals
 
 # Phantom App imports
-import inspect
 import phantom.app as phantom
-# from phtoolbox.app.base_connector import NiceBaseConnector, handle
 from phantom.action_result import ActionResult
 from phantom.base_connector import BaseConnector
-
 
 # Third-party
 import tdxlib
 from tdxlib import tdx_api_exceptions as tdx_ex
+from phtoolbox.app.base_connector import NiceBaseConnector, handle
 
 __version__ = 'GITHUB_TAG'
 __git_hash__ = 'GITHUB_SHA'
@@ -46,77 +44,6 @@ SEVERITY_TABLE = {
     "HIGH": "10204",
     "CRITICAL": "10542",
 }
-
-
-# start of base_connector.py
-
-# -----------------------------------------
-# Nice and dry Phantom App
-# -----------------------------------------
-
-
-def handle(*action_ids):
-    """Registor function to handle given action_ids."""
-    def decorator(func):
-        func._handle = action_ids
-        return func
-    return decorator
-
-
-class NiceBaseConnector():
-
-    def __init__(self, app_success_code, app_error_code):
-        self.__version__ = 'UNSET_VERSION - set self.__version__ to emit here'
-        self.__git_hash__ = \
-            'UNSET_GIT_HASH - set self.__git_hash__ to emit here'
-        self.__build_time__ = \
-            'UNSET_BUILD_TIME - set self.__build_time__ to emit here'
-
-        self.actions = {}
-        for _, method in inspect.getmembers(self):
-            if hasattr(method, '_handle'):
-                for action_id in getattr(method, '_handle'):
-                    self.actions[action_id] = method
-
-        self._state = None
-        self._app_success = app_success_code
-        self._app_error = app_error_code
-
-    def nice_handle_action(self, param):
-        """A function to implement handle_action.
-        The handle_action method MUST be defined in the child class."""
-        action_id = self.get_action_identifier()
-        self.debug_version_info()
-        self.debug_print("action_id", self.get_action_identifier())
-
-        if action_id in self.actions.keys():
-            return self.actions[action_id](param)
-
-        return self._app_error
-
-    def debug_version_info(self):
-        """ Developers are encouraged to set the following values:
-            - self.__version__ = 'GITHUB_TAG'
-            - self.__git_hash__ = 'GITHUB_SHA'
-            - self.__build_time__ = 'BUILD_TIME'
-        """
-
-        self.debug_print("Version: " + self.__version__)
-        self.debug_print("Git Hash: " + self.__git_hash__)
-        self.debug_print("Build Time:" + self.__build_time__)
-
-    def initialize(self):
-        # Load the state in initialize, use it to store data
-        # that needs to be accessed across actions
-        self._state = self.load_state()
-        return self._app_success
-
-    def finalize(self):
-        # Save the state, this data is saved across actions and
-        # app upgrades
-        self.save_state(self._state)
-        return self._app_success
-# end of base_connector.py
 
 
 class OrgNameAndEndpointSet(Exception):
